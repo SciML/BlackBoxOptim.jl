ef6015bd34bc630b7c6e0eb4e75ba06ab2bde0d1
Oscar Smith <oscardssmith@gmail.com>
2026-03-09 09:21:45 -0400

    remove unneeded const

--- test/helper.jl
+++ test/helper.jl
@@ -1,7 +1,7 @@
 using BlackBoxOptim
 using Test
 
-const NumTestRepetitions = 100
+NumTestRepetitions = 100
 
 random_candidate(n, lo, hi) =
     BlackBoxOptim.Candidate{Float64}([clamp(randn(), lo, hi) for _ in 1:n])
